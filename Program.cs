364cde4537312e424c8e4e2edb7d0f53adf9d750
Fekade Senbeta <fekadesenbeta@gmail.com>
2021-02-06 15:42:08 -0700

    arrays in C#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Arrays1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { -5 };
+            Test(a);
+            Console.WriteLine(a[0]);
+        }
+
+        static void Test(int[] a)
+        {
+            a[0] = 100;
+
+        }
+    }
+}
